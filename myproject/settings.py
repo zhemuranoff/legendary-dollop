53a6ec8fc85f2364b8e916c50598092e01894826
evgeniymuranov <zhe.muranoff@gmail.com>
2017-01-30 15:00:28 +0300

    lastChanges1

--- myproject/settings.py
+++ myproject/settings.py
@@ -85,9 +85,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 
 # Password validation
